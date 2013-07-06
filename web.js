000255359a1175c967b3f363f920cafefde11715
Stefan James <stefanjames@live.com>
2013-07-06 01:48:19 +0000

    Updated web.js again

--- web.js
+++ web.js
@@ -5,7 +5,7 @@ var app = express.createServer(express.logger());
 var content = fs.readFileSync("./index.html");
 
 app.get('/', function(request, response) {
-  response.send(redFileSync_string(content.toString("utf8");
+  response.send(content.toString("utf8"));
 });
 
 var port = process.env.PORT || 5000;
