c67ee2ecf5cab5fd71b7ca5700c5fdc28291cf76
Stefan James <stefanjames@live.com>
2013-07-06 01:11:12 +0000

    Updated web.js again

--- web.js
+++ web.js
@@ -1,9 +1,10 @@
 var express = require('express');
 
 var app = express.createServer(express.logger());
+var buffer = fs.readFileSync("./index.html", "uft8");
 
 app.get('/', function(request, response) {
-  response.send(fs.readFileSync("./index.html", "uft8"));
+  response.send(buffer);
 });
 
 var port = process.env.PORT || 5000;
