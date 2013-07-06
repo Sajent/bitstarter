630b66095d1fc6044f99586b4d9cca6a4c7f21c8
Stefan James <stefanjames@live.com>
2013-07-06 02:00:35 +0000

    Updated web.js again

--- web.js
+++ web.js
@@ -2,13 +2,11 @@ var express = require('express');
 
 var app = express.createServer(express.logger());
 
-var content = "hello"
-
 app.get('/', function(request, response) {
-  response.send("content");
+  response.send('Hello World!');
 });
 
-Var port = process.env.PORT || 5000;
+var port = process.env.PORT || 5000;
 app.listen(port, function() {
   console.log("Listening on " + port);
 });
