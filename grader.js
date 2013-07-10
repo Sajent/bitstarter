af55a6aae9f71c5bef60e0d34ebce67a51c87735
Stefan James <stefanjames@live.com>
2013-07-10 22:36:16 +0000

    Updated grader.js

--- grader.js
+++ grader.js
@@ -1,11 +1,22 @@
 #!/usr/bin/env node
 
-
+var sys = require('util');
+var rest = require('restler');
 var fs = require('fs');
 var program = require('commander');
 var cheerio = require('cheerio');
 var HTMLFILE_DEFAULT = "index.html";
 var CHECKSFILE_DEFAULT = "checks.json";
+ 
+var callThis = function(data, status) {
+if (data instanceof Error) {
+    sys.puts('Error: ' + data.message);
+    this.retry(5000); // try again after 5 sec
+  } else {
+    sys.puts(data);
+  }
+};
+
 
 var assertFileExists = function(infile) {
     var instr = infile.toString();
@@ -46,10 +57,13 @@ if(require.main == module) {
     program
 	.option('-c, --checks <check_file>', 'Path to checks.json', clone(assertFileExists), CHECKSFILE_DEFAULT)
 	.option('-f, --file <html_file>', 'Path to index.html', clone(assertFileExists), HTMLFILE_DEFAULT)
+        .option('-u, --url <URL>', 'http://evening-woodland-8597.herokuapp.com/')
 	.parse(process.argv);
     var checkJson = checkHtmlFile(program.file, program.checks);
     var outJson = JSON.stringify(checkJson, null, 4);
     console.log(outJson);
-} else {
+} 
+if(program.url){rest.get(program.url).on('complete', callThis); }
+else {
     exports.checkHtmlFile = checkHtmlFile;
 }
